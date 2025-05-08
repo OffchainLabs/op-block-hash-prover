de862ad5550409dbc55c3c4f4bd8d58fdc4214df
Henry <11198460+godzillaba@users.noreply.github.com>
2025-05-08 14:12:07 -0600

    set up as typescript module

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,9 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  paths: {
+    sources: "./src/contracts",
+  }
 };
 
 export default config;
---
+++ src/ts/index.ts
@@ -0,0 +1,9 @@
+export class Foo {
+  bar: string;
+  constructor() {
+    this.bar = "bar";
+  }
+  getBar() {
+    return this.bar;
+  }
+}
