42405e2e58938799680112c484a84170031ac7b0
Henry <11198460+godzillaba@users.noreply.github.com>
2025-05-09 15:22:09 -0600

    building

--- src/ts/ChildToParentProverHelper.ts
+++ src/ts/ChildToParentProverHelper.ts
@@ -1,17 +1,16 @@
-import { AbstractProvider, JsonRpcPayload, JsonRpcProvider, JsonRpcResult, Provider } from 'ethers';
-import {ChildToParentProver, ChildToParentProver__factory} from '../../typechain-types';
+import { Hash, Hex, PublicClient } from "viem";
 
 export type GetTargetBlockHashExtraArguments = undefined
 
 export class ChildToParentProverHelper {
-  constructor(public readonly childToParentProverAddress: string, readonly childProvider: Provider, readonly parentProvider: Provider) {}
+  constructor(public readonly childToParentProverAddress: string, readonly childClient: PublicClient, readonly parentClient: PublicClient) {}
 
   // return the newest block hash that can be returned by getTargetBlockHash on the prover
-  async buildInputForGetTargetBlockHash(): Promise<{ input: string, targetBlockHash: string }> {
+  async buildInputForGetTargetBlockHash(): Promise<{ input: Hex, targetBlockHash: Hash }> {
     throw new Error("Not implemented");
   }
 
-  async buildInputForVerifyTargetBlockHash(homeBlockHash: string): Promise<{ input: string, targetBlockHash: string }> {
+  async buildInputForVerifyTargetBlockHash(homeBlockHash: Hash): Promise<{ input: Hex, targetBlockHash: Hash }> {
     throw new Error("Not implemented");
   }
 }
