2445cd798b5952058afd123356b3d64461aaaedd
Henry <11198460+godzillaba@users.noreply.github.com>
2025-05-16 11:18:05 -0600

    move fork block num to tests

--- test/ChildToParentProver.test.ts
+++ test/ChildToParentProver.test.ts
@@ -13,12 +13,15 @@ import { ChildToParentProverHelper } from '../src/ts/prover-helper/ChildToParent
 import { ChildToParentProver$Type } from '../artifacts/src/contracts/ChildToParentProver.sol/ChildToParentProver'
 import { basicProverTests } from './basicProverTests'
 
-// replace this with the most recent child block hash available in the parent chain's state
+// replace this with the block number of the home chain fork test block
+const FORK_TEST_BLOCK=0x13f7f27c
+
+// replace this with the most recent target block hash available in the target chain's state
 // this is used to test the prover's ability to prove a block
 const MOST_RECENT_TARGET_CHAIN_BLOCK_HASH: Hash =
   '0x3bc1a497257a501e84e875bbe3e619bbdde267fc255162329e4b9df2c504386d'
 
-// replace this with a known storage slot value at the specified parent chain block hash
+// replace this with a known storage slot value at the specified target chain block hash
 // for example a token account balance
 const KNOWN_STORAGE_SLOT_ACCOUNT: Address =
   '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640'
@@ -35,7 +38,7 @@ describe('ChildToParentProver', function () {
   let helper: ChildToParentProverHelper
 
   beforeEach(async () => {
-    await reset(getEnv('CHILD_RPC_URL'), getEnv('CHILD_FORK_TEST_BLOCK'))
+    await reset(getEnv('CHILD_RPC_URL'), FORK_TEST_BLOCK)
 
     targetClient = createPublicClient({
       transport: http(getEnv('PARENT_RPC_URL')),
--- test/ParentToChildProver.test.ts
+++ test/ParentToChildProver.test.ts
@@ -13,12 +13,15 @@ import { ParentToChildProverHelper } from '../src/ts/prover-helper/ParentToChild
 import { ParentToChildProver$Type } from '../artifacts/src/contracts/ParentToChildProver.sol/ParentToChildProver'
 import { basicProverTests } from './basicProverTests'
 
-// replace this with the most recent child block hash available in the parent chain's state
+// replace this with the block number of the home chain fork test block
+const FORK_TEST_BLOCK=0x1568a70
+
+// replace this with the most recent target block hash available in the target chain's state
 // this is used to test the prover's ability to prove a block
 const MOST_RECENT_TARGET_CHAIN_BLOCK_HASH: Hash =
   '0x3c8f4a1b6599dfa00468e2609bb45f317ba5fa95e7ef198b03b75bebf54dd580'
 
-// replace this with a known storage slot value at the specified child chain block hash
+// replace this with a known storage slot value at the specified target chain block hash
 // for example a token account balance
 const KNOWN_STORAGE_SLOT_ACCOUNT: Address =
   '0xC6962004f452bE9203591991D15f6b388e09E8D0'
@@ -35,7 +38,7 @@ describe('ParentToChildProver', function () {
   let helper: ParentToChildProverHelper
 
   beforeEach(async () => {
-    await reset(getEnv('PARENT_RPC_URL'), getEnv('PARENT_FORK_TEST_BLOCK'))
+    await reset(getEnv('PARENT_RPC_URL'), FORK_TEST_BLOCK)
 
     targetClient = createPublicClient({
       transport: http(getEnv('CHILD_RPC_URL')),
